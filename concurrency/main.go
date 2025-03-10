c30f3fc87946b99aa553092637a36c50f384a6ce
Whatman0 <elone3acrew@gmail.com>
2025-03-10 13:05:48 +0500

    First homework
    First homework

---
+++ concurrency/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func main() {
+	numChan, resChan := make(chan int), make(chan int)
+	var wg sync.WaitGroup
+	for range 10 {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			rand.NewSource(time.Now().UnixNano())
+			num := rand.Intn(101)
+			numChan <- num
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			result := <-numChan
+			resChan <- result * result
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(resChan)
+		close(numChan)
+	}()
+	for square := range resChan {
+		fmt.Println(square)
+	}
+}
